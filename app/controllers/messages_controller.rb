35c57a56b5d5a4845b22a811b408cdb0831515a4
yohei.matsui <macchandx@gmail.com>
2019-07-24 15:28:46 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -12,24 +13,21 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @mesage = Message.new(message_params)
-    
-    if @mesage.save
-      flash[success] = 'Message が正常に投稿されました。'
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
       redirect_to @message
     else
-      flash.now[:danger] = 'Messageが投稿されませんでした。'
+      flash.now[:danger] = 'Message が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -40,15 +38,18 @@ class MessagesController < ApplicationController
   end
 
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
 
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
-  
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
